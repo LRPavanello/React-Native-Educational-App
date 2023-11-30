a5c35e9ae7c1f1007b3718c15356e0e22019081e
Luis R Pavanello <102007776+luisrpavanelli@users.noreply.github.com>
2023-11-29 21:16:45 -0300

    base

---
+++ Educational/App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textoAzul}>Educational App!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',  
+  },
+  textoAzul: {
+    color: 'blue',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+});
